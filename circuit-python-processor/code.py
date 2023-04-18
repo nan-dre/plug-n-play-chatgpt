7f9fb1b236b2ab5f9f8b5f653381037ed4f8a129
nan-dre <andu_niculae@yahoo.com>
2023-04-18 23:11:38 +0300

    Solve too many packets error after interrupting prompt

--- circuit-python-processor/code.py
+++ circuit-python-processor/code.py
@@ -67,7 +67,7 @@ def call_chatgpt(text):
     requests = adafruit_requests.Session(pool, ssl.create_default_context())
     full_prompt = [{"role": "user", "content": text},]
     text_response = ""
-    layout.write("\n")
+    stop = False
     with requests.post("https://api.openai.com/v1/chat/completions",
         json={"model": "gpt-3.5-turbo", "messages": full_prompt, "stream": True},
         headers={
@@ -78,10 +78,10 @@ def call_chatgpt(text):
                 for line in iter_lines(response):
                     # If the user wants to end the prompt early
                     if keyboard_uart.in_waiting:
-                        break
+                        stop = True
                     if line.startswith("data: [DONE]"):
                         break
-                    if line.startswith("data: "):
+                    if line.startswith("data: ") and not stop:
                         data = json.loads(line[5:])
                         word = data.get('choices')[0].get('delta').get('content')
                         if word is not None:
@@ -144,77 +144,84 @@ if __name__ == '__main__':
         if(len(current_uart_data) > 0 and current_uart_data[0] != 1):
             continue
         if len(current_uart_data) > 0 and current_uart_data[-1] == 255:
-            keycodes = []
-            modifiers = []
-            characters = []
+            packet_count = 0
+            for i in range(len(current_uart_data)):
+                if current_uart_data[i] == 255:
+                    packet_count += 1
+            packet_length = len(current_uart_data) // packet_count
+            packets = [current_uart_data[i:i+packet_length] for i in range(0, len(current_uart_data), packet_length)]
+            for packet in packets:
+                keycodes = []
+                modifiers = []
+                characters = []
 
-            L_modifiers_mask = current_uart_data[1] & 0b00001111
-            R_modifiers_mask = (current_uart_data[1] & 0b11110000) >> 4
-            L_modifiers = [L_MODIFIER_LIST[i] for i in range(len(L_MODIFIER_LIST)) if (L_modifiers_mask >> i) & 1]
-            R_modifiers = [R_MODIFIER_LIST[i] for i in range(len(R_MODIFIER_LIST)) if (R_modifiers_mask >> i) & 1]
-            modifiers.extend(L_modifiers)
-            modifiers.extend(R_modifiers)
-            keycodes.extend(modifiers)
+                L_modifiers_mask = packet[1] & 0b00001111
+                R_modifiers_mask = (packet[1] & 0b11110000) >> 4
+                L_modifiers = [L_MODIFIER_LIST[i] for i in range(len(L_MODIFIER_LIST)) if (L_modifiers_mask >> i) & 1]
+                R_modifiers = [R_MODIFIER_LIST[i] for i in range(len(R_MODIFIER_LIST)) if (R_modifiers_mask >> i) & 1]
+                modifiers.extend(L_modifiers)
+                modifiers.extend(R_modifiers)
+                keycodes.extend(modifiers)
 
-            for i in range(3, 8):
-                character = HID_KEYCODE_TO_ASCII[current_uart_data[i]][0]
-                if character == '\xcc':
-                    keycodes.append(Keycode.CAPS_LOCK)
-                elif character == '\xaa':
-                    keycodes.append(Keycode.RIGHT_ARROW)
-                elif character == '\xab':
-                    keycodes.append(Keycode.LEFT_ARROW)
-                elif character == '\xac':
-                    keycodes.append(Keycode.DOWN_ARROW)
-                elif character == '\xad':
-                    keycodes.append(Keycode.UP_ARROW)
-                elif character != 0:
-                    characters.append(character)
-                    keycodes.append(layout.keycodes(character)[0])
-            
-            print(current_uart_data)
-            print(keycodes)
+                for i in range(3, 8):
+                    character = HID_KEYCODE_TO_ASCII[packet[i]][0]
+                    if character == '\xcc':
+                        keycodes.append(Keycode.CAPS_LOCK)
+                    elif character == '\xaa':
+                        keycodes.append(Keycode.RIGHT_ARROW)
+                    elif character == '\xab':
+                        keycodes.append(Keycode.LEFT_ARROW)
+                    elif character == '\xac':
+                        keycodes.append(Keycode.DOWN_ARROW)
+                    elif character == '\xad':
+                        keycodes.append(Keycode.UP_ARROW)
+                    elif character != 0:
+                        characters.append(character)
+                        keycodes.append(layout.keycodes(character)[0])
+                
+                print(packet)
+                print(keycodes)
 
-            if keycodes == [Keycode.CONTROL, Keycode.ALT, Keycode.G]:
-                if listening_for_prompt == False:
-                    listening_for_prompt = True
-                    LED.value = True
-                else:
-                    listening_for_prompt = False
-                    LED.value = False
-                    call_api = True
-
-            pressed_keycodes = list_diff(keycodes, last_pressed_keycodes)
-            released_keycodes = list_diff(last_pressed_keycodes, keycodes)
-            pressed_characters = list_diff(characters, last_pressed_characters)
-            released_characters = list_diff(last_pressed_characters, characters)
-            for keycode in pressed_keycodes:
-                kbd.press(keycode)
-            for keycode in released_keycodes:
-                kbd.release(keycode)
-
-            if listening_for_prompt:
-                if(len(pressed_characters) > 0):
-                    # Exit if escape is pressed
-                    if pressed_characters[0] == '\x1b':
-                        print("Exiting listening mode and deleting history")
-                        current_prompt = ""
+                if keycodes == [Keycode.CONTROL, Keycode.ALT, Keycode.G]:
+                    if listening_for_prompt == False:
+                        listening_for_prompt = True
+                        LED.value = True
+                    else:
                         listening_for_prompt = False
                         LED.value = False
-                    elif pressed_characters[0] == '\x08':
-                        current_prompt = current_prompt[:-1]
-                    elif (Keycode.CONTROL or Keycode.ALT or Keycode.GUI) not in keycodes:
-                            if Keycode.SHIFT in keycodes:
-                                current_prompt += pressed_characters[0].upper()
-                            else:
-                                current_prompt += pressed_characters[0]
+                        call_api = True
 
-            last_pressed_keycodes = keycodes
-            last_pressed_characters = characters
+                pressed_keycodes = list_diff(keycodes, last_pressed_keycodes)
+                released_keycodes = list_diff(last_pressed_keycodes, keycodes)
+                pressed_characters = list_diff(characters, last_pressed_characters)
+                released_characters = list_diff(last_pressed_characters, characters)
+                for keycode in pressed_keycodes:
+                    kbd.press(keycode)
+                for keycode in released_keycodes:
+                    kbd.release(keycode)
 
-            if call_api == True and keycodes == []:
-                call_api = False
-                print(current_prompt)
-                current_prompt += call_chatgpt(current_prompt)
+                if listening_for_prompt:
+                    if(len(pressed_characters) > 0):
+                        # Exit if escape is pressed
+                        if pressed_characters[0] == '\x1b':
+                            print("Exiting listening mode and deleting history")
+                            current_prompt = ""
+                            listening_for_prompt = False
+                            LED.value = False
+                        elif pressed_characters[0] == '\x08':
+                            current_prompt = current_prompt[:-1]
+                        elif (Keycode.CONTROL or Keycode.ALT or Keycode.GUI) not in keycodes:
+                                if Keycode.SHIFT in keycodes:
+                                    current_prompt += pressed_characters[0].upper()
+                                else:
+                                    current_prompt += pressed_characters[0]
+
+                last_pressed_keycodes = keycodes
+                last_pressed_characters = characters
+
+                if call_api == True and keycodes == []:
+                    call_api = False
+                    print(current_prompt)
+                    current_prompt += call_chatgpt(current_prompt)
 
             current_uart_data = bytearray()
