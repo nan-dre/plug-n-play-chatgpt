2d294a06d9bf9d9f9ea70a3166a71304aaf9c4cd
nan-dre <andu_niculae@yahoo.com>
2023-05-25 22:28:12 +0300

    Add new script

--- send_to_serial.bat
+++ send_to_serial.bat
@@ -1,5 +1,13 @@
-powershell.exe -command "Get-Clipboard > ./text.txt"
-mode COM7: baud=115200 data=8 parity=n stop=1 xon=on to=off 
-type text.txt >COM7
-mode COM7: baud=115200 data=8 parity=n stop=1 xon=off to=off 
-mode COM7: baud=115200 data=8 parity=n stop=1 xon=off to=off 
+setlocal enabledelayedexpansion
+set /a chunk_size=200
+set "filename=text.txt"
+
+powershell.exe -command "Get-Clipboard > %filename%"
+pause
+
+for /F "usebackq delims=" %%A in (`powershell -Command "Get-Content -Raw -Path '%filename%' | foreach { $chunk = $_.ToCharArray(); $i = 0; while ($i -lt $chunk.Length) { $start = $i; $end = $i + %chunk_size% - 1; if ($end -ge $chunk.Length) { $end = $chunk.Length - 1 }; $chunk[$start..$end] -join ''; $i += %chunk_size% } }"`) do (
+    mode COM7: baud=115200 data=8 parity=n stop=1 xon=on to=off 
+    echo %%A >COM7
+    mode COM7: baud=115200 data=8 parity=n stop=1 xon=off to=off 
+    mode COM7: baud=115200 data=8 parity=n stop=1 xon=off to=off 
+)
