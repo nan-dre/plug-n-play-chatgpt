d9bde7f6f11df87ea75962ca32f815a217e0f556
nan-dre <andu_niculae@yahoo.com>
2023-05-29 16:57:50 +0300

    Add powershell script for sending clipboard through tcp

---
+++ send_clipboard_to_server.ps1
@@ -0,0 +1,40 @@
+Function Send-TCPMessage { 
+    Param ( 
+            [Parameter(Mandatory=$true, Position=0)]
+            [ValidateNotNullOrEmpty()] 
+            [string] 
+            $EndPoint
+        , 
+            [Parameter(Mandatory=$true, Position=1)]
+            [int]
+            $Port
+        , 
+            [Parameter(Mandatory=$true, Position=2)]
+            [string]
+            $Message
+    ) 
+    Process {
+        # Setup connection 
+        $IP = [System.Net.Dns]::GetHostAddresses($EndPoint) 
+        $Address = [System.Net.IPAddress]::Parse($IP) 
+        $Socket = New-Object System.Net.Sockets.TCPClient($Address,$Port) 
+    
+        # Setup stream wrtier 
+        $Stream = $Socket.GetStream() 
+        $Writer = New-Object System.IO.StreamWriter($Stream)
+
+        # Write message to stream
+        $Message | % {
+            $Writer.WriteLine($_)
+            $Writer.Flush()
+        }
+    
+        # Close connection and stream
+        $Stream.Close()
+        $Socket.Close()
+    }
+}
+
+# shortcut target C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -WindowStyle hidden -ExecutionPolicy Bypass -File C:\Users\Andy\Downloads\sent_to_server.ps1
+$clipboard_text = Get-Clipboard
+Send-TCPMessage -Port 5000 -Endpoint 192.168.0.66 -message $clipboard_text
